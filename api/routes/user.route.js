b8fadbd914106fcfb67ab80feed19d4e7636b900
Diona Ymeraga <ymeragadiona@gmail.com>
2025-02-20 23:48:12 +0100

    Added Save Post Functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -13,8 +13,11 @@ import { verifyToken } from "../middleware/verifyToken.js";
 const router = express.Router();
 
 router.get("/", getUsers);
-router.get("/search/:id", verifyToken, getUser);
+// router.get("/search/:id", verifyToken, getUser);
 router.put("/:id", verifyToken, updateUser);
 router.delete("/:id", verifyToken, deleteUser);
+router.post("/save", verifyToken, savePost);
+router.get("/profilePosts", verifyToken, profilePosts);
+router.get("/notification", verifyToken, getNotificationNumber);
 
 export default router;
